a3c1b24b1ab8f131956d57f2fd9a9e2dfcc5790b
Huda <mhuda_ies@yahoo.com>
2021-07-03 12:05:38 +0400

    Add project files.

---
+++ Attendance/Branch.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/User.master" AutoEventWireup="true" CodeFile="Branch.aspx.cs" Inherits="Branch" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+
+<asp:Content ID="Content2" runat="server" contentplaceholderid="ContentPlaceHolder1">
+</asp:Content>
+
+
---
+++ Attendance/User.master.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class User : System.Web.UI.MasterPage
+{
+    DS_Staff.STAFF_SELECTDataTable StaffDT = new DS_Staff.STAFF_SELECTDataTable();
+    DS_StaffTableAdapters.STAFF_SELECTTableAdapter staffadpter = new DS_StaffTableAdapters.STAFF_SELECTTableAdapter();
+
+    DS_student.StudentMstDataTable studentdt = new DS_student.StudentMstDataTable();
+    DS_studentTableAdapters.StudentMstTableAdapter studentadapter = new DS_studentTableAdapters.StudentMstTableAdapter();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblstafferror.Text = "";
+        lblstuerror.Text = "";
+    }
+
+
+
+    protected void btnstaff_Click(object sender, EventArgs e)
+    {
+        StaffDT = staffadpter.stafflogin(txtstaffpass.Text, txtstaffuname.Text);
+        if (StaffDT.Rows.Count == 1)
+        {
+            Session["Email"] = txtstaffuname.Text;
+            Response.Redirect("staff/Default.aspx");
+        }
+        else
+        {
+            lblstafferror.Text = "تسجيل الدخول خطأ";
+        }
+    }
+
+    protected void btnstuentry_Click(object sender, EventArgs e)
+    {
+        studentdt = studentadapter.studentlogin(txtstupass.Text, txtstuuname.Text);
+        if(studentdt.Rows.Count ==1)
+        {
+            Session["Uname"] = txtstuuname.Text;
+            Response.Redirect("Student/Default.aspx");
+
+        }
+        else
+        {
+            lblstuerror.Text = "خطأ في تسجيل الدخول";
+        }
+    }
+}
